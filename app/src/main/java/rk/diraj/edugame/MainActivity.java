e9e8b89d4a50eac1b589518f4144851f4294503f
Diraj RK <diraj.ravikumar@gmail.com>
2017-05-24 18:57:51 +0800

    Replaced buttons with Action Bar/Tool Bar menu items.

--- app/src/main/java/rk/diraj/edugame/MainActivity.java
+++ app/src/main/java/rk/diraj/edugame/MainActivity.java
@@ -3,12 +3,14 @@ package rk.diraj.edugame;
 // CP3406 Assignment 2 by Diraj Ravikumar (13255244)
 
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-
+import android.view.Menu;
+import android.view.MenuItem;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -16,14 +18,14 @@ import android.content.Intent;
 public class MainActivity extends AppCompatActivity implements OnClickListener{
 
     MediaPlayer mediaPlayer;
-    private Button playBtn, settingBtn, helpBtn, scoreBtn;
+    private Button playBtn, helpBtn;
     private String[] levelNames = {"Easy", "Medium", "Hard"};
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // Hides the action bar
-        getSupportActionBar().hide();
+//        getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -33,14 +35,10 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
         mediaPlayer.start();
         // Buttons
         playBtn = (Button) findViewById(R.id.playBtn);
-        settingBtn = (Button) findViewById(R.id.settingBtn);
         helpBtn = (Button) findViewById(R.id.helpBtn);
-        scoreBtn = (Button) findViewById(R.id.scoreBtn);
         // Button OnClickListener
         playBtn.setOnClickListener(this);
-        settingBtn.setOnClickListener(this);
         helpBtn.setOnClickListener(this);
-        scoreBtn.setOnClickListener(this);
     }
 
     @Override
@@ -53,34 +51,11 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
             this.startActivity(playIntent);
         }
 
-        else if(view.getId()==R.id.settingBtn){
-            //Settings button launches an Alert Dialog which presents difficulty options
-
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-            builder.setTitle("Choose a level")
-                    .setSingleChoiceItems(levelNames, 0, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-                            //Start Game
-                            startPlay(which);
-                        }
-                    });
-            AlertDialog ad = builder.create();
-            ad.show();
-        }
-
         else if(view.getId()==R.id.helpBtn){
             //How To button launches Help Activity
 
             Intent helpIntent = new Intent(this, HelpActivity.class);
             this.startActivity(helpIntent);
-        }
-        else if(view.getId()==R.id.scoreBtn){
-            //Scores button launches Scores Activity
-
-            Intent scoreIntent = new Intent(this, ScoreActivity.class);
-            this.startActivity(scoreIntent);
         }
     }
 
@@ -114,5 +89,44 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
         mediaPlayer.stop();
         mediaPlayer.release();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.highscore) {
+            Intent intent = new Intent(this, ScoreActivity.class);
+            startActivity(intent);
+        }
+
+        else if (id == R.id.settings) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            builder.setTitle("Choose a level")
+                    .setSingleChoiceItems(levelNames, 0, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            //Start Game
+                            startPlay(which);
+                        }
+                    });
+            AlertDialog ad = builder.create();
+            ad.show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
 }
